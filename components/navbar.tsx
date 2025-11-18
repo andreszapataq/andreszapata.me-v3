e8461c610d28a84c14d2f43bc3ce3f86fa41f9ad
andreszapataq <andreszapataq@gmail.com>
2025-11-17 23:24:44 -0500

    update logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,13 +20,13 @@ export default function Navbar() {
                 <Link href="/" aria-label="Home">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 67.96 48"
+                        viewBox="0 0 69.43 48"
                         className="h-6 w-auto"
                         fill="currentColor"
                         aria-hidden="true"
                         >
-                            <path d="M0,42.46L24.43,8.34H.6V1.34h34.18v5.54L10.41,40.99h25.57v7.01H0v-5.54Z"/>
-                            <path d="M48.47,0h6.68l12.82,22.16h-8.08l-8.08-14.29-8.08,14.29h-8.08L48.47,0Z"/>
+                            <path d="M0,41.26L22.83,9.81H.6V1.34h34.78v6.81l-22.7,31.38h24.03v8.48H0v-6.74Z"/>
+                            <path d="M48.4,0h8.01l13.02,22.43h-9.68l-7.34-13.09-7.34,13.09h-9.68L48.4,0Z"/>
                     </svg>
                 </Link>
             </div>
