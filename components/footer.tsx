08934651ad1cf275f34b6655dd8673ec7743167f
andreszapataq <andreszapataq@gmail.com>
2025-12-27 21:13:39 -0500

    get full year in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="mt-25 py-6 border-t border-gray-200">
       <div className="px-6 md:px-23 text-sm text-gray-600">
-        © 2025 Andres Zapata · {" "}
+        © {new Date().getFullYear()} Andres Zapata · {" "}
         <Link href="mailto:contacto@andreszapata.me" className="hover:underline">
           Email
         </Link>
