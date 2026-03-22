11198369bc8c4ce982176f3d630cf341dd5fa5ec
andreszapataq <andreszapataq@gmail.com>
2026-03-21 21:52:58 -0500

    update timeline dots

--- components/propuestas/proposal-timeline.tsx
+++ components/propuestas/proposal-timeline.tsx
@@ -17,15 +17,18 @@ export default function ProposalTimeline({
         <span className="font-semibold text-foreground">{totalDuration}</span>
       </p>
 
-      <div className="relative pl-6 mt-2">
-        {/* Vertical line */}
-        <div className="absolute left-[7px] top-2 bottom-2 w-px bg-gray-200 print:bg-gray-300" />
-
+      <div className="relative pl-8 mt-2">
         <div className="space-y-6">
           {phases.map((phase, i) => (
             <div key={i} className="relative">
-              {/* Dot */}
-              <div className="absolute -left-6 top-1.5 w-[15px] h-[15px] rounded-full border-2 border-gray-300 bg-white print:border-gray-400" />
+              {/* Vertical line (not on last item) */}
+              {i < phases.length - 1 && (
+                <div className="absolute -left-[21px] top-[22px] -bottom-6 w-px bg-gray-200 print:bg-gray-300" />
+              )}
+              {/* Step number */}
+              <div className="absolute -left-8 top-0.5 w-[22px] h-[22px] rounded-full bg-gray-200 flex items-center justify-center text-xs font-semibold text-gray-600 print:bg-gray-100 print:text-gray-500">
+                {i + 1}
+              </div>
 
               <div>
                 <div className="flex flex-wrap items-baseline gap-2">
